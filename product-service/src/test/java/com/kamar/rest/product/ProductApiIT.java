ba3a9351d0bd2cc5ba6014a50be05a2b8f86cf86
Kamardoblibennani@gmail.com <kamardoblibennani@gmail.com>
2025-12-17 03:17:33 +0100

    Flyway established + tests successful No testcontainers ( used local postgre database ) through docker

--- product-service/src/test/java/com/kamar/rest/product/ProductApiIT.java
+++ product-service/src/test/java/com/kamar/rest/product/ProductApiIT.java
@@ -1,41 +1,33 @@
 package com.kamar.rest.product;
 
 import com.kamar.rest.product.dto.ProductDTO;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
+import org.springframework.test.context.ActiveProfiles;
+import org.flywaydb.core.Flyway;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Testcontainers
+@ActiveProfiles("test")
 class ProductApiIT {
-
-    @SuppressWarnings("resource")
-	@Container
-    static PostgreSQLContainer<?> postgres =
-            new PostgreSQLContainer<>("postgres:16")
-                    .withDatabaseName("productdb")
-                    .withUsername("test")
-                    .withPassword("test");
-
-    @DynamicPropertySource
-    static void datasourceConfig(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", postgres::getJdbcUrl);
-        registry.add("spring.datasource.username", postgres::getUsername);
-        registry.add("spring.datasource.password", postgres::getPassword);
-    }
 
     @Autowired
     private TestRestTemplate restTemplate;
+
+    @Autowired
+    private Flyway flyway;
+
+    @BeforeEach
+    void resetDatabase() {
+        flyway.clean();
+        flyway.migrate();
+    }
 
     @Test
     void should_create_and_get_product() {
@@ -53,7 +45,6 @@ class ProductApiIT {
                 restTemplate.getForEntity("/products", ProductDTO[].class);
 
         assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(getResponse.getBody()).isNotNull();
         assertThat(getResponse.getBody()).hasSize(1);
     }
 }
